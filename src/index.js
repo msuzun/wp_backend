10808f4c4d100a6b4cf7dbe9970431a0c70c9f70
msuzun <sevki_uzun53@hotmail.com>
2025-05-10 17:39:23 +0300

    SIGTERM signal closing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import logger from "./config/logger.config.js";
 
 //env variables
 const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV);
 
-app.listen(PORT,()=>{
+let server;
+server = app.listen(PORT,()=>{
     logger.info(`server is listening at ${PORT}`);
 });
+
+//handle server errors
+const exitHandler = () => {
+    if(server){  
+        logger.info("Server closed");
+        process.exit(1);
+    }
+    else{
+        process.exit(1);
+    }
+}
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on("uncaughtException",unexpectedErrorHandler);
+
+process.on("unhandledRejection",unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM",()=>{
+    if(server){  
+        logger.info("Server closed");
+        process.exit(1);
+    }
+})
